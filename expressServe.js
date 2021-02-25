447346921fb68f94fbb1a721d088f13163644517
jiyehan <mwldpm@naver.com>
2021-02-25 14:41:31 +0900

    Add page of balance and transaction

--- expressServe.js
+++ expressServe.js
@@ -4,6 +4,8 @@ const request = require('request');
 const app = express()
 const jwt = require('jsonwebtoken');
 const auth = require("./lib/auth")
+const companyId = "M202111589U"
+var moment = require('moment');
 // json타입에 데이터 전송을 허용한다
 app.use(express.json());
 //form타입에 데이터 전송을 허용한다
@@ -45,6 +47,12 @@ app.get('/signup', function (req, res) {
 app.get('/login', function (req, res) {
   res.render('login');
 })
+app.get('/main', function (req, res) {
+  res.render('main');
+})
+app.get('/balance', function (req, res) {
+  res.render('balance');
+})
 app.get('/authTest', auth, function (req, res) {
   res.send('정상적으로 로그인 되셨습니다.');
 })
@@ -71,7 +79,7 @@ app.get('/authResult', function (req, res) {
       }
       else {
         var accessRequestResult = JSON.parse(body);
-        console.log(accessRequestResult);
+        // console.log(accessRequestResult);
         res.render('resultChild', { data: accessRequestResult });
       }
   })
@@ -85,7 +93,7 @@ app.post('/signup', function(req, res) {
   var userRefreshToken = req.body.userRefreshToken;
   var userSeqNo = req.body.userSeqNo;
 
-  console.log(userName, userEmail, userPassword, userAccessToken);
+  // console.log(userName, userEmail, userPassword, userAccessToken);
   var sql = "INSERT INTO user (name, email, password, accesstoken, refreshtoken, userseqno) VALUES (?,?,?,?,?,?)";
   connection.query(sql,[userName, userEmail, userPassword, userAccessToken, userRefreshToken, userSeqNo], function (err, result) {
       if(err){
@@ -101,7 +109,7 @@ app.post('/signup', function(req, res) {
 app.post('/login', function(req, res){
   var userEmail = req.body.userEmail;
   var userPassword = req.body.userPassword;
-  console.log(userEmail, userPassword)
+  // console.log(userEmail, userPassword)
   var sql = "SELECT * FROM user WHERE email = ?";
   connection.query(sql, [userEmail], function(err, result){
       if(err){
@@ -135,7 +143,6 @@ app.post('/login', function(req, res){
                   )            
               }
               else {
-
                   res.json(2);
               }
           }
@@ -143,6 +150,121 @@ app.post('/login', function(req, res){
   })
 
 })
+app.post('/list', auth, function (req, res) {
+  var user = req.decoded;
+  var sql = "SELECT * FROM user WHERE id = ?";
+  connection.query(sql,[user.userId], function(err, result){
+    if(err) throw err;
+    else {
+      var dbUserData = result[0];
+      // console.log('db**********', dbUserData);
+      var option = {
+          method : "GET",
+          url : "https://testapi.openbanking.or.kr/v2.0/user/me",
+          headers : {
+              Authorization : "Bearer " + dbUserData.accesstoken
+          },
+          qs : {
+              user_seq_no : dbUserData.userseqno
+          }
+      }
+      request(option, function(err, response, body){
+          if(err){
+              console.error(err);
+              throw err;
+          }
+          else {
+              var listRequestResult = JSON.parse(body);
+              res.json(listRequestResult)
+          }
+      })        
 
+    }
+  })
+})
+app.post('/balance', auth, function (req, res) {
+  var user = req.decoded;
+  var finusenum = req.body.fin_use_num
+  var sql = "SELECT * FROM user WHERE id = ?";
+  var countnum = Math.floor(Math.random() * 1000000000) + 1;
+  var transId = companyId + countnum;
+  // console.log(transId);
+  var transdtime = moment(new Date()).format('YYYYMMDDhhmmss');
+
+  connection.query(sql,[user.userId], function(err, result){
+    if(err) throw err;
+    else {
+      var dbUserData = result[0];
+      var option = {
+          method : "GET",
+          url : "https://testapi.openbanking.or.kr/v2.0/account/balance/fin_num",
+          headers : {
+              Authorization : "Bearer " + dbUserData.accesstoken
+          },
+          qs : {
+            bank_tran_id: transId,
+            fintech_use_num: finusenum,
+            tran_dtime: transdtime
+          }
+      }
+      request(option, function(err, response, body){
+          if(err){
+              console.error(err);
+              throw err;
+          }
+          else {
+              var balanceRequestResult = JSON.parse(body);
+              res.json(balanceRequestResult)
+          }
+      })        
+
+    }
+  })
+})
+app.post('/transactionList', auth, function (req, res) {
+  var user = req.decoded;
+  var finusenum = req.body.fin_use_num
+  var sql = "SELECT * FROM user WHERE id = ?";
+  var countnum = Math.floor(Math.random() * 1000000000) + 1;
+  var transId = companyId + countnum;
+  // console.log(transId);
+  var transdtime = moment(new Date()).format('YYYYMMDDhhmmss');
+
+  connection.query(sql,[user.userId], function(err, result){
+    if(err) throw err;
+    else {
+      var dbUserData = result[0];
+      var option = {
+          method : "GET",
+          url : "https://testapi.openbanking.or.kr/v2.0/account/transaction_list/fin_num",
+          headers : {
+              Authorization : "Bearer " + dbUserData.accesstoken
+          },
+          qs : {
+            bank_tran_id: transId,
+            fintech_use_num: finusenum,
+            tran_dtime: transdtime,
+            inquiry_type: "A",
+            inquiry_base: "D",
+            from_date: "20210224",
+            to_date: "20210225",
+            sort_order: "D"
+
+          }
+      }
+      request(option, function(err, response, body){
+          if(err){
+              console.error(err);
+              throw err;
+          }
+          else {
+              var transactionRequestResult = JSON.parse(body);
+              res.json(transactionRequestResult)
+          }
+      })        
+
+    }
+  })
+})
 
 app.listen(3000)
